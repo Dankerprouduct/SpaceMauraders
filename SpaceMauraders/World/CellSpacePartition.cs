da35a5e241f2011686a0f6c0ef37cdb2e7e4961f
David William Fitzgerald <Dankerprouduct@users.noreply.github.com>
2018-10-13 10:17:30 -0400

    hopefully this doesnt fuck my shit up

---
+++ SpaceMauraders/World/CellSpacePartition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceMauraders.World
+{
+    public class CellSpacePartition
+    {
+        // List<Entity> entities = new List<SpaceMauraders.Entity>(); 
+
+        public CellSpacePartition() 
+        {
+
+        }
+
+        public CellSpacePartition( int partitionSize)
+        {
+
+        }
+
+
+
+    }
+}
